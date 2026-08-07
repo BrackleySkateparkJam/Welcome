11646f4adc81365cb383d490cbe0b9cefb07dca4
BrackleySkateparkJam <brackleyskatejam@gmail.com>
2026-08-07 10:12:52 +0100

    Update Leaderboard.js

--- js/Leaderboard.js
+++ js/Leaderboard.js
@@ -55,7 +55,7 @@ function renderLeaderboard(data) {
   sports.forEach(function(sport) {
 
     let html = `
-      <details class="sport-section" closed>
+      <details class="sport-section">
 
         <summary class="sport-title">
           🏆 ${sport}
